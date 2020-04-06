7869ec97e52e0374f84f3156fafbe56ab66cdb1e
Robert <robt15@hotmail.com>
2020-04-06 13:13:04 -0400

    started jwt config

--- src/main/java/com/roberttaylor/shoply/controller/ClientController.java
+++ src/main/java/com/roberttaylor/shoply/controller/ClientController.java
@@ -1,16 +1,11 @@
 package com.roberttaylor.shoply.controller;
 
-import java.security.Principal;
-import java.util.Base64;
-
-import javax.servlet.http.HttpServletRequest;
-
 import com.roberttaylor.shoply.dao.ClientDAO;
 import com.roberttaylor.shoply.entities.Client;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,24 +14,9 @@ public class ClientController {
 
     @Autowired
     private ClientDAO clientDAO;
+    
+    @PostMapping("user")
+    public Client login(@RequestParam("username") String username, @RequestParam("password") String pwd){
 
-    @RequestMapping("/signup")
-    public Client addClient(@RequestBody Client client) {
-        Client newClient = clientDAO.save(client);
-        return newClient;
-    }
-
-    @RequestMapping("/login")
-    public boolean login(@RequestBody Client client) {
-        return
-            client.getUsername().equals("user") && client.getPassword().equals("password");
-    }
-
-    @RequestMapping("/user")
-    public Principal client(HttpServletRequest request) {
-        String authToken = request.getHeader("Authorization")
-            .substring("Basic".length()).trim();
-        return () -> new String(Base64.getDecoder()
-            .decode(authToken)).split(":")[0];
     }
 }
--- src/main/java/com/roberttaylor/shoply/dao/ClientDAO.java
+++ src/main/java/com/roberttaylor/shoply/dao/ClientDAO.java
@@ -7,5 +7,5 @@ import com.roberttaylor.shoply.entities.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClientDAO extends JpaRepository<Client, UUID> {
-    
+    Client findByUsername(String username);
 }
